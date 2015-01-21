30e53e7df1eb636ce419daad04efb3df6062a9ad
Kody Peterson <kodypeterson91@gmail.com>
2015-01-20 21:02:44 -0500

    get error

--- helpers/checkMessages.js
+++ helpers/checkMessages.js
@@ -18,6 +18,7 @@ module.exports = function(){
             }).then(function(){
                 resolve();
             }).catch(function(error){
+                if(error) throw error;
                 resolve();
             });
         });
